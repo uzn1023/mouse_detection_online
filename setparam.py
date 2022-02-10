8a16e158ed911009b88eeef41f5121d852d2924e
uzn1023 <e5305@gm.kochi-ct.jp>
2022-02-10 12:51:58 +0900

    Add proc img

--- setparam.py
+++ setparam.py
@@ -4,6 +4,8 @@ import numpy as np
 import PySimpleGUI as sg
 from serial.tools import list_ports
 import serial
+import copy
+import movement
 
 def setparam():
     param = []
@@ -13,21 +15,22 @@ def setparam():
         if cap1.isOpened(): 
             cameras.append(i1)
         cap1.release()
-
+    
     # Arduino ポートリスト取得
     ports = list_ports.comports()    # ポートデータを取得
     devices = [info.device for info in ports]
+    devices.append("None")
     print("Found cameras: " + str(cameras))
     print ("Founds devides: " + str(devices))
     # GUI preparation
     sg.theme('Black')
-    txt_cmr = sg.Text('Select Camera',size=(15,1), background_color='#F7F3EC', text_color='#000')
+    txt_cmr = sg.Text('Selected Camera',size=(15,1))
     cmb_cmr = sg.Combo(cameras, default_value=cameras[0],size=(10, 1), readonly=True, key="cmr_select")
-    txt_ald = sg.Text('Select Arduino',size=(15,1), background_color='#F7F3EC', text_color='#000')
+    txt_ald = sg.Text('Selected Arduino',size=(15,1))
     cmb_ald = sg.Combo(devices, default_value=devices[0],size=(10, 1), readonly=True, key="ald_select")
     column = [txt_cmr, cmb_cmr],\
              [txt_ald, cmb_ald]
-    layout = [[sg.Column(column, background_color='#F7F3EC')], [sg.Image(filename='', key='frame'), sg.Image(filename='', key='image')],[sg.Button('Done', size=(10, 1), font='Helvetica 14')]]
+    layout = [[sg.Column(column), sg.Image(filename='', key='frame'), sg.Image(filename='', key='image')],[sg.Button('Done', size=(10, 1), font='Helvetica 14')]]
     window = sg.Window('SelectDevices', layout, no_titlebar=False, location=(0, 0), resizable=True)
     CAMERA = cameras[0]
     capture = cv2.VideoCapture(CAMERA)
@@ -39,6 +42,7 @@ def setparam():
     ser.setDTR(False)
     ser.open()
     outputval = 0
+    frame_old = None
     while(1):
         event, values = window.read(timeout=0)
         if CAMERA != int(values["cmr_select"]):
@@ -47,17 +51,30 @@ def setparam():
         ret, frame = capture.read()
         framebytes = cv2.imencode('.png', frame)[1].tobytes()
         window['frame'].update(data=framebytes)
-        if DEVICE != values["ald_select"]:
+        if values["ald_select"] != "None":
+            if DEVICE != values["ald_select"]:
+                DEVICE = str(values["ald_select"])
+                ser.port = DEVICE
+                ser.bandrate = 9600
+                ser.setDTR(False)
+                ser.open()
+            outputval += 25
+            if outputval > 255:
+                outputval = 0
+            ser.write(outputval.to_bytes(1, 'big'))
+        else:
             DEVICE = str(values["ald_select"])
-            ser.port = DEVICE
-            ser.bandrate = 9600
-            ser.setDTR(False)
-            ser.open()
-        outputval += 25
-        if outputval > 255:
-            outputval = 0
-        ser.write(outputval.to_bytes(1, 'big'))
 
+        if frame_old is None:   # 一番最初に取得したフレームをframe_oldとして保存
+            frame_old = copy.copy(frame)
+        else:
+            # 2回目以降はframe, frame_oldの２つを処理プログラムに投げる
+            f_out, dif_out = movement.proc(frame_old, frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            frame_old = copy.copy(frame)
+            imagebytes = cv2.imencode('.png', f_out)[1].tobytes()
+            window['image'].update(data=imagebytes)
         if event in ('Done', None):
             param.append(CAMERA)
             print("Selected camera: " + str(CAMERA))
